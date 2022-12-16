0f00fd03b78ee4a8ee9dbf448151c183663cd00d
nscarab <cherser@list.ru>
2022-12-16 19:26:33 +0300

    4 in progress

---
+++ level02/Resource/decoder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python -c 'print "756e505234376848".decode("hex")[::-1]'
+python -c 'print "45414a3561733951".decode("hex")[::-1]'
+python -c 'print "377a7143574e6758".decode("hex")[::-1]'
+python -c 'print "354a35686e475873".decode("hex")[::-1]'
+python -c 'print "48336750664b394d".decode("hex")[::-1]'
